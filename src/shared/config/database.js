8370fea791043e5f246f53fc5d3178ce2abff181
johnldev <john.lenon.dev@gmail.com>
2021-11-24 15:59:35 -0300

    fix: env variables

--- src/shared/config/database.js
+++ src/shared/config/database.js
@@ -1,20 +1,20 @@
 
+const { config } = require('dotenv')
+config()
 const isOffline = process.env.IS_OFFLINE === 'true';
-
 const db = isOffline ? {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
   userName: process.env.DB_USER,
   password:  process.env.DB_PASSWORD,
-  database:  process.env.DB_PASSWORD,
+  database:  process.env.DB_NAME,
 } : {
   host: process.env.AURORA_HOST,
   port: process.env.AURORA_PORT || 3306,
   userName: process.env.AURORA_USERNAME,
   password:  process.env.AURORA_PASSWORD,
-  database:  process.env.AURORA_PASSWORD,
+  database:  process.env.AURORA_DB_NAME,
 }
-
 module.exports = {
   dialect: 'mysql',
   host: db.host,
